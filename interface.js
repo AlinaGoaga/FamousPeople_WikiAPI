93e08dfff7856a915e7e9aec187716d0d0ae0752
Alina Goaga <goaga.alina@gmail.com>
2019-03-05 16:35:28 +0000

    Extracted the ApiData class to a separate file

--- interface.js
+++ interface.js
@@ -1,41 +1,4 @@
-require("isomorphic-fetch");
-
-const name = process.argv.slice(2)[0]
-
-function getWikiData(name) {
-
-  const HTMLParser = require('fast-html-parser');
-  const urlAPI = `https://en.wikipedia.org/w/api.php?action=parse&page=${name}&format=json`;
-  const otherParams = {
-    method: "GET"
-  };
-
-  fetch(urlAPI,otherParams)
-  .then(data=>{return data.json()})
-  .then(res=>{
-    return res.parse.text['*']
-  })
-  .then(result=>{
-    return HTMLParser.parse(result)
-  })
-  .then(parsed=>{
-    console.log(display(parsed,".mw-redirect"))
-    console.log(display(parsed,".bday"))
-    console.log(display(parsed,".birthplace"))
-    console.log(display(parsed,".deathplace"))
-    // console.log(display(parsed,".mw-parser-output"))
-  })
-
-}
-
-function display(parsed, selector) {
-
-  if (parsed.querySelector(selector) != null) {
-    return parsed.querySelector(selector).text
-  } else {
-    return "Unavailable"
-  }
-
-}
-
-getWikiData(name);
+const ApiData = require('./src/ApiData');
+instance = new ApiData();
+const name = process.argv.slice(2)[0];
+instance.getWikiData(name);
---
+++ src/ApiData.js
@@ -0,0 +1,37 @@
+require("isomorphic-fetch");
+
+class ApiData {
+
+  getWikiData(name) {
+
+    const HTMLParser = require('fast-html-parser');
+    const urlAPI = `https://en.wikipedia.org/w/api.php?action=parse&page=${name}&format=json`;
+    const otherParams = { method: "GET" };
+
+    fetch(urlAPI,otherParams)
+    .then(data=>{return data.json()})
+    .then(res=>{return res.parse.text['*']})
+    .then(result=>{return HTMLParser.parse(result)})
+    .then(parsed=>{
+      console.log(this.display(parsed,".mw-redirect"))
+      console.log(this.display(parsed,".bday"))
+      console.log(this.display(parsed,".birthplace"))
+      console.log(this.display(parsed,".deathplace"))
+      // console.log(display(parsed,".mw-parser-output"))
+    })
+
+  }
+
+  display(parsed, selector) {
+
+    if (parsed.querySelector(selector) != null) {
+      return parsed.querySelector(selector).text
+    } else {
+      return "Unavailable"
+    }
+
+  }
+
+}
+
+module.exports = ApiData;
