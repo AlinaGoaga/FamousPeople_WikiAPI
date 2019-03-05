da8c0b313fc85cf29080472c8379ab02da8bb08a
Alina Goaga <goaga.alina@gmail.com>
2019-03-05 09:34:48 +0000

    Data being returned for spouse

--- interface.js
+++ interface.js
@@ -1,23 +1,33 @@
 require("isomorphic-fetch");
 
-  function getWikiData() {
+function getWikiData() {
 
-    const HTMLParser = require('fast-html-parser');
-    const urlAPI = "https://en.wikipedia.org/w/api.php?action=parse&page=Seth_Rogen&format=json";
-    const otherParams = {
-      method: "GET"
-    };
+  const HTMLParser = require('fast-html-parser');
+  const urlAPI = "https://en.wikipedia.org/w/api.php?action=parse&page=Seth_Rogen&format=json";
+  const otherParams = {
+    method: "GET"
+  };
 
-    fetch(urlAPI,otherParams)
-    .then(data=>{return data.json()})
-    .then(res=>{
-      return res.parse.text['*']
-    })
-    .then(result=>{
-      return HTMLParser.parse(result)
-    })
-    .then(parsed=>{
-      console.log(parsed);
-    })
+  fetch(urlAPI,otherParams)
+  .then(data=>{return data.json()})
+  .then(res=>{
+    return res.parse.text['*']
+  })
+  .then(result=>{
+    return HTMLParser.parse(result)
+  })
+  .then(parsed=>{
+    console.log(display(parsed,".mw-redirect"))
+  })
+
+}
+
+function display(parsed, selector) {
+
+  if (parsed.querySelector(".mw-redirect") != null) {
+    return parsed.querySelector(".mw-redirect").text
+  }
+
+}
 
 getWikiData();
